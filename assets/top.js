03ca430845df0f7a5705f6016965c02297f557bc
masa <te09115t@yahoo.co.jp>
2019-02-25 15:37:36 +0900

    first commit.

---
+++ assets/top.js
@@ -0,0 +1,28 @@
+// UTF-8
+/*----------------------------------------------
+*
+* 	jQuery ready
+*
+----------------------------------------------*/
+
+$(function(){
+
+	//mv
+	// $('#mvImg').nivoSlider({
+	// 	effect: 'fade',
+	// 	slices: 1,
+	// 	boxCols: 1,
+	// 	boxRows: 1,
+	// 	animSpeed: 500,
+	// 	pauseTime: 3000,
+	// 	directionNav: false,
+	// 	controlNav: false,
+	// 	pauseOnHover: false,
+	// });
+	$('#youtube,#youtube02').colorbox({
+		// iframe:true,
+		// innerWidth:855,
+		// innerHeight:481
+	});
+
+});
